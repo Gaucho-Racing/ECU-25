4725922274849327177e974d58ff72ff88a4a575
Daniel Hansen <dchansen06@gmail.com>
2025-01-09 19:20:49 -0800

    Add info to state operations

--- Application/Src/stateMachine.c
+++ Application/Src/stateMachine.c
@@ -4,101 +4,101 @@ void stateMachineTick(State* state, InformationToPassToState info)
 {
     switch(*state) {
         case GLV_OFF:
-            glv_off(state);
+            glv_off(state, info);
             break;
         case GLV_ON:
-            glv_on(state);
+            glv_on(state, info);
             break;
         case PRECHARGE_ENGAGED:
-            precharge_engaged(state);
+            precharge_engaged(state, info);
             break;
         case PRECHARGING:
-            precharging(state);
+            precharging(state, info);
             break;
         case PRECHARGE_COMPLETE:
-            precharge_complete(state);
+            precharge_complete(state, info);
             break;
         case DRIVE_STANDBY:
-            drive_standby(state);
+            drive_standby(state, info);
             break;
         case DRIVE_ACTIVE_IDLE:
-            drive_active_idle(state);
+            drive_active_idle(state, info);
             break;
         case DRIVE_ACTIVE_POWER:
-            drive_active_power(state);
+            drive_active_power(state, info);
             break;
         case DRIVE_ACTIVE_REGEN:
-            drive_active_regen(state);
+            drive_active_regen(state, info);
             break;
         case TS_DISCHARGE_OFF:
-            ts_discharge_off(state);
+            ts_discharge_off(state, info);
             break;
         case REFLASH_TUNE:
-            reflash_tune(state);
+            reflash_tune(state, info);
             break;
         case ERROR:
         default:
-            error(state);
+            error(state, info);
         break;
     }
 }
 
-void glv_off(State* state)
+void glv_off(State* state, InformationToPassToState info)
 {
 
 }
 
-void glv_on(State* state)
+void glv_on(State* state, InformationToPassToState info)
 {
 
 }
 
-void precharge_engaged(State* state)
+void precharge_engaged(State* state, InformationToPassToState info)
 {
 
 }
 
-void precharging(State* state)
+void precharging(State* state, InformationToPassToState info)
 {
 
 }
 
-void precharge_complete(State* state)
+void precharge_complete(State* state, InformationToPassToState info)
 {
 
 }
 
-void drive_standby(State* state)
+void drive_standby(State* state, InformationToPassToState info)
 {
 
 }
 
-void drive_active_idle(State* state)
+void drive_active_idle(State* state, InformationToPassToState info)
 {
 
 }
 
-void drive_active_power(State* state)
+void drive_active_power(State* state, InformationToPassToState info)
 {
 
 }
 
-void drive_active_regen(State* state)
+void drive_active_regen(State* state, InformationToPassToState info)
 {
 
 }
 
-void ts_discharge_off(State* state)
+void ts_discharge_off(State* state, InformationToPassToState info)
 {
 
 }
 
-void reflash_tune(State* state)
+void reflash_tune(State* state, InformationToPassToState info)
 {
     
 }
 
-void error(State* state)
+void error(State* state, InformationToPassToState info)
 {
 
 }
