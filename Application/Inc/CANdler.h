2cb9628eb8e498db528e5db1f9131b247a7be782
Casey Zwicker <caseypzwicker@gmail.com>
2025-01-23 13:39:28 -0800

    CANdler stub

--- Application/Inc/CANdler.h
+++ Application/Inc/CANdler.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-void handleCANMessage(uint16_t msgID, uint8_t *data, uint32_t length, uint32_t timestamp);
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp);
 
 #endif // CANDLER_H
--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -1,9 +1,36 @@
 #include "CANdler.h"
 #include "stateMachine.h"
+#include "msgIDs.h"
 
 
-void handleCANMessage(uint16_t msgID, uint8_t *data, uint32_t length, uint32_t timestamp) {
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
     switch(msgID) {
+        case MSG_DEBUG:
+            break;
+        case MSG_PING:
+            break;
+        case MSG_ACU_STATUS:
+            break;
+        case MSG_ACU_CELL_DATA_1:
+            break;
+        case MSG_ACU_CELL_DATA_2:
+            break;
+        case MSG_ACU_CELL_DATA_3:
+            break;
+        case MSG_ACU_CELL_DATA_4:
+            break;
+        case MSG_ACU_CELL_DATA_5:
+            break;
+        case MSG_LV_DC_DC_STATUS:
+            break;
+        case MSG_DTI_INVERTER_STATUS:
+            break;
+        case MSG_GR_INVERTER_STATUS:
+            break;
+        case MSG_FAN_STATUS:
+            break;
+
+
         /* update globals in stateMachine */
     }
 }
