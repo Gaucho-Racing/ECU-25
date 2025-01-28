f3115eb91a54d8f994476d8753a8c6202968bc32
Daniel Hansen <dchansen06@gmail.com>
2025-01-27 17:04:57 -0800

    Typo sorry

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -3,7 +3,7 @@
 #include "msgIDs.h"
 #include <stdbool.h>
 
-void handleCANMe16sage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
     switch(msgID) {
         case MSG_DEBUG:
             if (length != 64) {
