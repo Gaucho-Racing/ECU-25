f2a665eaed8202324d40e049f0dd7ed23a6caf81
Daniel Hansen <dchansen06@gmail.com>
2025-01-27 19:19:16 -0800

    Move to header

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -3,35 +3,6 @@
 #include "msgIDs.h"
 #include <stdbool.h>
 
-typedef struct {    // Since it is so big it would be nice to have a struct handle it instead of manually going through it
-    uint8_t Accumulator_Voltage;
-    uint8_t TS_Voltage;
-    uint8_t Accumulator_Current;
-    uint8_t Accumulator_SOC;
-    uint8_t GLV_SOC;
-    uint8_t V_20v_Voltage;
-    uint8_t V_12v_Voltage;
-    uint8_t SDC_Voltage;
-    uint8_t Min_Cell_Voltage;
-    uint8_t Max_Cell_Temp;
-    uint8_t Over_Temp_Error;
-    uint8_t Over_Voltage_Error;
-    uint8_t Under_Voltage_Error;
-    uint8_t Over_Current_Error;
-    uint8_t Under_Current_Error;
-    uint8_t Under_Voltage_20v_Warning;
-    uint8_t Under_Voltage_12v_Warning;
-    uint8_t Under_Voltage_SDC_Warning;
-    uint8_t Precharge_Error;
-    uint8_t IRminus_Precharge_State;
-    uint8_t IRplus_State;
-    uint8_t Software_Latch;
-    uint8_t HV_Input_Voltage;
-    uint8_t HV_Output_Voltage;
-    uint8_t HV_Input_Current;
-    uint8_t HV_Output_Current;
-} ACU_Status_Msg;
-
 void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
     switch(msgID) {
         case MSG_DEBUG:
