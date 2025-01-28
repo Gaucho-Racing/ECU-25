d652b031598cd359549ab06b1ce59b353997490e
Daniel Hansen <dchansen06@gmail.com>
2025-01-27 16:42:50 -0800

    Fix errors in test.c

--- Application/Test/test.c
+++ Application/Test/test.c
@@ -1,12 +1,15 @@
 // Testing LED lights on STM32
 
+#include <stdint.h>
+#include "stm32g4xx_hal.h"
+
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 void ledControl();
 
 GPIO_InitTypeDef led;
 GPIO_PinState ledState;
-int ledArray[] = {15, 14, 13, 12, 11, 10, 9, 8}
+int ledArray[] = {15, 14, 13, 12, 11, 10, 9, 8};
 uint32_t count;
 
 int main() {
@@ -15,7 +18,7 @@ int main() {
     SystemClock_Config();
     MX_GPIO_Init();
 
-    led.Pin = GPIO_PIN_ALL;
+    led.Pin = GPIO_PIN_All;
     led.Mode = GPIO_MODE_OUTPUT_PP;
     led.Pull = GPIO_NOPULL;
     led.Speed = GPIO_SPEED_FREQ_LOW;
