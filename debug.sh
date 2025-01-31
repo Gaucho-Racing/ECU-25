25de35436307dce8beb3c397794d405e8c779461
Daniel Hansen <dchansen06@gmail.com>
2025-01-30 19:55:40 -0800

    Typo

--- debug.sh
+++ debug.sh
@@ -9,7 +9,7 @@ if [ "$1" != "build" ] && [ "$1" != "b" ]
 then
 	openocd -f interface/stlink.cfg -f target/stm32g4x.cfg -c "program ECU-25.elf" &
 	OCDPID=$!
-	unset -e
+	set +e
 	gdb --symbols=ECU-25.elf --eval-command="target extended-remote :3333"
 	kill $OCDPID
 else
