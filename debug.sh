d929807f53c7479df9f191397d736182a3899b94
Daniel Hansen <dchansen06@gmail.com>
2025-01-30 19:57:03 -0800

    Update debug.sh
    Co-authored-by: Bailey Say <baileyasay@gmail.com>
    Co-authored-by: ahoysal <ahoysal@users.noreply.github.com>
    Co-authored-by: Jason Wang <JayWango@users.noreply.github.com>

--- debug.sh
+++ debug.sh
@@ -10,7 +10,7 @@ then
 	openocd -f interface/stlink.cfg -f target/stm32g4x.cfg -c "program ECU-25.elf" &
 	OCDPID=$!
 	set +e
-	gdb --symbols=ECU-25.elf --eval-command="target extended-remote :3333"
+	arm-none-eabi-gdb --symbols=ECU-25.elf --eval-command="target extended-remote :3333"
 	kill $OCDPID
 else
 	echo "Ninja exited with status ${exitcode}"
