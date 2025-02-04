ab816684cef1ee5c35045f0c9a3c6c1686740e9b
Daniel Hansen <dchansen06@gmail.com>
2025-02-03 19:48:07 -0800

    Add todo
    Co-authored-by: Bailey Say <baileyasay@gmail.com>
    Co-authored-by: ahoysal <ahoysal@users.noreply.github.com>
    Co-authored-by: Jason Wang <JayWango@users.noreply.github.com>
    Co-authored-by: ManasijaBhargavan <ManasijaBhargavan@users.noreply.github.com>
    Co-authored-by: kzwicker <kzwicker@users.noreply.github.com>

--- debug.sh
+++ debug.sh
@@ -9,6 +9,7 @@ if [ "$1" != "build" ] && [ "$1" != "b" ]
 then
 	openocd -f interface/stlink.cfg -f target/stm32g4x.cfg -c "program ECU-25.elf" &
 	OCDPID=$!
+	#TODO Handle openocd failure correctly
 	set +e
 	arm-none-eabi-gdb --symbols=ECU-25.elf --eval-command="target extended-remote :3333"
 	kill $OCDPID
