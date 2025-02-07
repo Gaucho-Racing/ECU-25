9ea64f6a07f67aea94cc9955335cd3a1d9bbf6d7
ManasijaBhargavan <manas@ucsb.edu>
2025-02-07 10:49:06 -0800

    Update utils.h

--- Application/Inc/utils.h
+++ Application/Inc/utils.h
@@ -7,11 +7,14 @@
 #define TICK_FREQ 100000 // overflow after 12 hrs runtime teehee
 
 uint32_t millis(void);
-uint8_t getBit(uint8_t number, uint8_t indexFromRight);
-uint8_t get2Bits(uint8_t number, uint8_t indexFromRight);
-uint8_t getBits(uint8_t number, uint8_t indexFromRight, uint8_t length);
+
+uint8_t getBit(uint8_t number, uint8_t indexFromLeft);
+uint8_t getBits(uint8_t number, uint8_t indexFromLeft, uint8_t length);
+
 void setSoftwareLatch(uint8_t close);
+
 uint8_t ACUError(ACU_Status_MsgTwo *acuMsgTwo);
 uint8_t GRIError(Inverter_Status_Msg_Three *msgGriThree);
 uint8_t ACUWarning(ACU_Status_MsgTwo *acuMsgTwo);
+
 #endif // UTILS_H
