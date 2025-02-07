4a58250775ff7b36487742205fed26daac72de8c
ManasijaBhargavan <manas@ucsb.edu>
2025-02-07 10:47:47 -0800

    Update utils.c

--- Application/Src/utils.c
+++ Application/Src/utils.c
@@ -8,19 +8,14 @@ uint32_t millis(void) {
     return HAL_GetTick() * (1000/TICK_FREQ);
 }
 
-uint8_t getBit(uint8_t number, uint8_t indexFromRight)
+uint8_t getBit(uint8_t number, uint8_t indexFromLeft)
 {
-    return (number >> (7 - indexFromRight)) & 0x01;
+    return (number >> (7 - indexFromLeft)) & 0x01;
 }
 
-uint8_t get2Bits(uint8_t number, uint8_t indexFromRight)
+uint8_t getBits(uint8_t number, uint8_t indexFromLeft, uint8_t length)
 {
-    return (number >> (7 - indexFromRight)) & 0x03; // 0b11
-}
-
-uint8_t getBits(uint8_t number, uint8_t indexFromRight, uint8_t length)
-{
-    return (number >> (7 - indexFromRight)) & (pow(length, 2) - 1);
+    return (number >> (8 - indexFromLeft - length)) & ((1 << length) - 1);
 }
 
 void setSoftwareLatch(uint8_t close)
