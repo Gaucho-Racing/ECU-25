629ce317456b34b7b6e8c8c73d4a6d1e15662cb3
Daniel Hansen <dchansen06@gmail.com>
2025-02-13 19:37:15 -0800

    Add friendly space

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -303,6 +303,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
             }
             
             globalSteerSettings = *(SteerSettings*)data;
+
             globalStatus.PowerLevelTorqueMap = globalSteerSettings.torqueMapCurrentEncoder;
     }
 }
