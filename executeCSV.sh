9d7e11dd8d7d0c510afdc97cdcb8537009c9b311
Daniel Hansen <dchansen06@gmail.com>
2025-02-18 20:31:52 -0800

    Finished csv integration

--- executeCSV.sh
+++ executeCSV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e;
 
-# Setup method to update the CSV files here
+# Setup method to update the CSV files 
 
 python3 CSV-25/grCSVtoH.py CSV-25/GR-ID.csv Application/Inc/grIDs.h
 python3 CSV-25/msgCSVtoH.py CSV-25/MSG-ID.csv Application/Inc/msgIDs.h
