2bbdf3ddd6c47cf99f86eae8ffac4480fc61ddbc
Daniel Hansen <dchansen06@gmail.com>
2025-02-18 20:56:24 -0800

    Now debug.sh exits if openocd fails

--- debug.sh
+++ debug.sh
@@ -8,11 +8,13 @@ exitcode="${?}"
 if [ "$1" != "build" ] && [ "$1" != "b" ]
 then
 	openocd -f interface/stlink.cfg -f target/stm32g4x.cfg -c "program ECU-25.elf" &
+
 	OCDPID=$!
+	sleep 1
 	ps -p $OCDPID > /dev/null 2>&1
+
 	if [ $? -eq 0 ]
 	then
-		#set +e
 		arm-none-eabi-gdb --symbols=ECU-25.elf --eval-command="target extended-remote :3333"
 		kill $OCDPID
 	fi
