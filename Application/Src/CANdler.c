24fcb35bee035a979e329b7c2e623bcb691192d5
ManasijaBhargavan <manas@ucsb.edu>
2025-02-27 19:41:05 -0800

    ignore

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -125,26 +125,26 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
             }
 
             // If IR- ever becomes 0 while not in GLV_ON or PRECHARGE_ENGAGED, that is a precharge cancellation and it must start discharging.
-            if(getBit(acuMsgTwo->Precharge_Errror_IR_State_Software_Latch_Bits, 0) == 0x0 && (globalStatus.ECUState != GLV_ON) && (globalStatus.ECUState != PRECHARGE_ENGAGED))
+            if(getBit(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 0) == 0x0 && (globalStatus.ECUState != GLV_ON) && (globalStatus.ECUState != PRECHARGE_ENGAGED))
             {
                 globalStatus.ECUState = TS_DISCHARGE_OFF;
             }
 
             // If it is precharging with IR- closed and then IR+ goes closed as well, precharge is complete (success confirmation)
             // IR+ -> 1 is precharge success confirmation
-            if((getBits(acuMsgTwo->Precharge_Errror_IR_State_Software_Latch_Bits, 1, 3) == 0x3) && (globalStatus.ECUState == PRECHARGING))
+            if((getBits(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 1, 3) == 0x3) && (globalStatus.ECUState == PRECHARGING))
             {
                 globalStatus.ECUState = PRECHARGE_COMPLETE;
             }
 
             //If IR+ ever opens on or after the precharging complete state, start discharging
-            if(getBit(acuMsgTwo->Precharge_Errror_IR_State_Software_Latch_Bits, 2) == 0x0 && globalStatus.ECUState != GLV_ON && globalStatus.ECUState != PRECHARGE_ENGAGED && globalStatus.ECUState != PRECHARGING)
+            if(getBit(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 2) == 0x0 && globalStatus.ECUState != GLV_ON && globalStatus.ECUState != PRECHARGE_ENGAGED && globalStatus.ECUState != PRECHARGING)
             {
                 globalStatus.ECUState = TS_DISCHARGE_OFF;
             }
 
             //If ACU software latch ever opens IR- ever opens while IR+ is closed, something has gone wrong
-            if(getBit(acuMsgTwo->Precharge_Errror_IR_State_Software_Latch_Bits, 3) == 0x0 || getBits(acuMsgTwo->Precharge_Errror_IR_State_Software_Latch_Bits, 1, 3) == 0x2)
+            if(getBit(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 3) == 0x0 || getBits(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 1, 3) == 0x2)
             {
                 globalStatus.ECUState = TS_DISCHARGE_OFF;
             }
