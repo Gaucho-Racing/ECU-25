13b6c52647b7b6fe400dd3b76a0e6dc4f7cd2c7f
Daniel Hansen <dchansen06@gmail.com>
2025-03-28 15:29:46 -0700

    Boolean operation

--- Application/Src/utils.c
+++ Application/Src/utils.c
@@ -28,7 +28,7 @@ void setSoftwareLatch(bool close)
     {
         HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_SET);
     }
-    else if (close == 0 && HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))
+    else if (!close && HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))
     {
         HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_RESET);
     }
