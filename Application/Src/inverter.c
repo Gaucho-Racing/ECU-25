351cd485bbad0a62ec5e9dc98876b44f11ccbc75
Daniel Hansen <dchansen06@gmail.com>
2025-03-28 14:26:37 -0700

    Removed extra functions

--- Application/Src/inverter.c
+++ Application/Src/inverter.c
@@ -42,18 +42,6 @@ void controlInverters(bool driveEnable)
 // See https://github.com/Gaucho-Racing/VDM-24/blob/main/src/Nodes.h for all of below
 // There is exactly 1 DTI per https://discord.com/channels/756738476887638107/1256764134872060007/1355067348423540757
 
-// uint32_t getERPM(void) {return(((uint32_t)inverterData.data[0][0] << 24) + ((uint32_t)inverterData.data[0][1] << 16) + ((uint32_t)inverterData.data[0][2] << 8) + inverterData.data[0][3]);} //rpm/pole pairs
-// float getDuty(void) {return((((uint32_t)inverterData.data[0][4] << 8) + inverterData.data[0][5])/10);} //i think [0,100]. Related to top speed
-// uint32_t getVoltIn(void) {return(((uint32_t)inverterData.data[0][6] << 8) + inverterData.data[0][7]);}
-// float getACCurrent(void) {return((float)(((uint16_t)(inverterData.data[1][0]) << 8) + inverterData.data[1][1])/10.0);}
-// float getDCCurrent(void) {return(((uint32_t)((uint16_t)(inverterData.data[1][2]) << 8) + inverterData.data[1][3])/10.0);}
-// float getInvTemp(void) {return((((uint32_t)inverterData.data[2][0] << 8) + inverterData.data[2][1])/10.0);} //Deg C
-// float getMotorTemp(void) {return((((uint32_t)inverterData.data[2][2] << 8) + inverterData.data[2][3])/10.0);} //Deg C
-// uint8_t getFaults(void) {return inverterData.data[2][4];}
-// float getCurrentD(void) {return((((uint32_t)inverterData.data[3][0] << 24) + ((uint32_t)inverterData.data[3][1] << 16) + ((uint32_t)inverterData.data[3][2] << 8) + inverterData.data[3][3])/100.0);}  //FOC current (don't need)
-// float getCurrentQ(void) {return((((uint32_t)inverterData.data[3][4] << 24) + ((uint32_t)inverterData.data[3][5] << 16) + ((uint32_t)inverterData.data[3][6] << 8) + inverterData.data[3][7])/100.0);}  //FOC current (don't need)
-uint8_t getThrottleIn(void) {return inverterData.data[4][0];}  //Received throttle signal by the invertor
-uint8_t getBrakeIn(void) {return inverterData.data[4][1];}  //Received brake signal by the invertor
 bool getD1(void) {return ((inverterData.data[4][2] & 0x80) == 0x80);}  //Digital input read
 bool getD2(void) {return ((inverterData.data[4][2] & 0x40) == 0x40);}  //Digital input read
 bool getD3(void) {return ((inverterData.data[4][2] & 0x20) == 0x20);}  //Digital input read
@@ -62,7 +50,6 @@ bool getDO1(void) {return ((inverterData.data[4][2] & 0x08) == 0x08);}  //Digita
 bool getDO2(void) {return ((inverterData.data[4][2] & 0x04) == 0x04);}  //Digital output write
 bool getDO3(void) {return ((inverterData.data[4][2] & 0x02) == 0x02);}  //Digital output write
 bool getDO4(void) {return ((inverterData.data[4][2] & 0x01) == 0x01);}  //Digital output write
-bool getDriveEnable(void) {return ((inverterData.data[4][3] & 0x01) == 0x01);} //These are setting that can be changed (prob don't need these)
 bool getCapTempLim(void) {return ((inverterData.data[4][4] & 0x80) == 0x80);}//         ^
 bool getDCCurrentLim(void) {return ((inverterData.data[4][4] & 0x40) == 0x40);}//       ^
 bool getDriveEnableLim(void) {return ((inverterData.data[4][4] & 0x20) == 0x20);}//     ^
@@ -74,4 +61,3 @@ bool getMotorTempLim(void) {return ((inverterData.data[4][4] & 0x01) == 0x01);}/
 bool getRPMMinLimit(void) {return ((inverterData.data[4][5] & 0x80) == 0x80);}//        ^
 bool getRPMMaxLimit(void) {return ((inverterData.data[4][5] & 0x40) == 0x40);}//        ^
 bool getPowerLimit(void) {return ((inverterData.data[4][5] & 0x20) == 0x20);}//         ^
-// FIXME Why are so many of these bools? (Copied from VDM-24 as such)
