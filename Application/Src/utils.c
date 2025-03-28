4c2d1dc2b73424f5f907e91ef87de8aa0a4e6b95
ManasijaBhargavan <manas@ucsb.edu>
2025-03-28 15:25:23 -0700

    software latch

--- Application/Src/utils.c
+++ Application/Src/utils.c
@@ -24,7 +24,7 @@ void setSoftwareLatch(uint8_t close)
     {
         HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_SET);
     }
-    else if (HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))
+    else if (close == 0 && HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))
     {
         HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_RESET);
     }
