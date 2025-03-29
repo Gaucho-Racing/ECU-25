0f65a25f39516d917458a340901516b9017b0021
Daniel Hansen <dchansen06@gmail.com>
2025-03-28 22:10:03 -0700

    Potentially big issue spotted, marked TODO

--- Application/Src/stateMachine.c
+++ Application/Src/stateMachine.c
@@ -11,6 +11,7 @@
 #include "msgIDs.h"
 #include "grIDs.h"
 
+// TODO Confirm valid following of the map/scale of values
 volatile StatusLump globalStatus = {
     .ECUState = GLV_ON,
     .StatusBits = {0},
