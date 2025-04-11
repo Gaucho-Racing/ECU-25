189cfbd63a3e688e1c341e43e33005e1a251658d
ManasijaBhargavan <manas@ucsb.edu>
2025-04-10 20:27:52 -0700

    changes to regenbuttonmap

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -14,8 +14,7 @@
 #include "utils.h"
 
 volatile uint8_t errorFlagBitsCan = 0;
-volatile uint8_t globalSteeringStatusRegen = 0;
-volatile uint8_t globalSteeringStatusButtonMap = 0;
+volatile uint8_t globalSteeringStatusRegenButtonMap = 0;
 
 void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length)
 {
@@ -328,7 +327,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
             break;
 
         case MSG_STEERING_STATUS:
-            if (length != 4) {
+            if (length != 2) {
                 numberOfBadMessages++;
                 return;
             } else {
@@ -339,9 +338,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
             globalStatus.PowerLevelTorqueMap = msgSteer->Current_Torque_Map_Encoder;
 
             /* Do not write to these values elsewhere! */
-            globalSteeringStatusRegen = msgSteer->regen;
-            globalSteeringStatusButtonMap = msgSteer->buttonMap;
-
+            globalSteeringStatusRegenButtonMap = msgSteer->regenButtonMap;
             // Handle buttons / regen here
             break;
 
