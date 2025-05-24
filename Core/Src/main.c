4c4d6d2a143c5fe99c64cae8ff959bab8f7e8c38
Daniel Hansen <dchansen06@gmail.com>
2025-05-24 02:13:55 -0700

    Enter newline

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -123,6 +123,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
     //writeMessage(DataBusCAN, MSG_DTI_CONTROL_12, GR_DTI_INVERTER, &enabled, 1);                       // 1 Drive Enable
     writeMessage(PrimaryBusCAN, MSG_DTI_CONTROL_1, GR_DTI_INVERTER, (uint8_t*)&targetAcCurrent, 2); // 20A Target AC Current
