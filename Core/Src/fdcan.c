1bbb23bf82560155bb5a9a62460287aeb920fa91
Daniel Hansen <dchansen06@gmail.com>
2025-05-25 23:33:40 -0700

    Wrong comment

--- Core/Src/fdcan.c
+++ Core/Src/fdcan.c
@@ -38,7 +38,6 @@ FDCAN_TxHeaderTypeDef TxHeader = {
 
 void writeDtiMessage(uint16_t msgID, uint8_t data[], uint32_t len)
 {
-  //                         1 byte              2 bytes        1 byte
     TxHeader.Identifier = (msgID << 8) | (GR_DTI_INVERTER);
     TxHeader.DataLength = len;
 
