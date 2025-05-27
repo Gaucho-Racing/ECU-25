4f5aef8772f0e2d6510037e5032ee73282ae1557
ManasijaBhargavan <manas@ucsb.edu>
2025-05-27 12:39:03 -0700

    fix

--- Application/Src/driving.c
+++ Application/Src/driving.c
@@ -32,7 +32,7 @@ void drive_standby(void)
 {
     controlInverters(true);
 
-    float throttle2 = (float) analogRead(APPS2_SIGNAL) / (ADC_MAX);
+    float throttle2 = (float) analogRead(APPS2_SIGNAL) * ADC_CONV;
     float pedalTravel = (throttle2 - THROTTLE_MIN_2) / (THROTTLE_MAX_2 - THROTTLE_MIN_2);
 
     if(BSE_APPS_violation && pedalTravel < APPS_DEADZONE){
