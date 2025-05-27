2e1002a01ad44e65733990420ab05bbe7e1f2a11
Daniel Hansen <dchansen06@gmail.com>
2025-05-26 20:49:36 -0700

    Normalized

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -123,7 +123,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    LOGOMATIC("APPS1 is %lf\t\tAPPS2 is %lf\n", analogRead(APPS1_SIGNAL) * (double)ADC_CONV, analogRead(APPS2_SIGNAL) * (double)ADC_CONV);
+    LOGOMATIC("APPS1 is %d\t\tAPPS2 is %d\n", analogRead(APPS1_SIGNAL), analogRead(APPS2_SIGNAL));
 
     //writeMessage(PrimaryBusCAN, MSG_DEBUG_2_0, GR_ALL, (uint8_t*)"Testing", 8);
 
