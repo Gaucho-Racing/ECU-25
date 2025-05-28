11720d2f0dcbe224875f049065ce0622784a1f16
Daniel Hansen <dchansen06@gmail.com>
2025-05-27 20:55:49 -0700

    Remove extra lines

--- Application/Src/inverter.c
+++ Application/Src/inverter.c
@@ -38,10 +38,6 @@ void sendInverterCommand(void)
         // LOGOMATIC("Global Inverter Settings 3 -- Set DC Current %d\n", globalInverterSettings[2].Set_DC_Current);
         // LOGOMATIC("Global Inverter Settings 3 -- RPM Limit %d\n", globalInverterSettings[2].RPM_Limit);
         // LOGOMATIC("Global Inverter Settings 3 -- Drive Enable %d\n", globalInverterSettings[2].Drive_Enable);
-//      LOGOMATIC("Global Inverter Settings 4 -- Set AC Current %d\n", globalInverterSettings[3].Set_AC_Current);   // Enable iff fourth motor/inverter
-//      LOGOMATIC("Global Inverter Settings 4 -- Set DC Current %d\n", globalInverterSettings[3].Set_DC_Current);   // Enable iff fourth motor/inverter
-//      LOGOMATIC("Global Inverter Settings 4 -- RPM Limit %d\n", globalInverterSettings[3].RPM_Limit);             // Enable iff fourth motor/inverter
-//      LOGOMATIC("Global Inverter Settings 4 -- Drive Enable %d\n", globalInverterSettings[3].Drive_Enable);       // Enable iff fourth motor/inverter
     }
 }
 
@@ -50,7 +46,6 @@ void controlInverters(bool driveEnable)
     globalInverterSettings[0] = (InverterSettings){0, 0, 0, (uint8_t)driveEnable};
     globalInverterSettings[1] = (InverterSettings){0, 0, 0, (uint8_t)driveEnable};
     globalInverterSettings[2] = (InverterSettings){0, 0, 0, (uint8_t)driveEnable};
-//  globalInverterSettings[3] = (InverterSettings){0, 0, 0, (uint8_t)driveEnable};  // Enable iff fourth motor/inverter
 
     sendInverterCommand();
 }
