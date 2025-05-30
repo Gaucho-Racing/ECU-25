69bcee45394a1b892655b80e70da97383c76379a
ManasijaBhargavan <manas@ucsb.edu>
2025-05-29 22:09:50 -0700

    slope changes

--- Application/Src/utils.c
+++ Application/Src/utils.c
@@ -42,8 +42,9 @@ void setSoftwareLatch(bool close)
 
 bool checkBSEAPPSviolation(float throttle1, float throttle2, float getPedalTravel, float brake)
 {
+    // End to end factor: 1.886089, offset = 0.2053413
     //Checks 2 * APPS_1 is within 10% of APPS_2 and break + throttle at the same time
-    return fabs(throttle2 - throttle1 * 1.7589 - 0.21923) > throttle2 * 0.1 || (brake >= BSE_DEADZONE && getPedalTravel >= 0.25);
+    return fabs(throttle2 - throttle1 * 1.91245 - 0.194428785) > throttle2 * 0.1 || (brake >= BSE_DEADZONE && getPedalTravel >= 0.25);
 }
 
 void validateForwardTorqueRequest(int16_t* tqr)
