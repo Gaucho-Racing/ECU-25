be14ffcd9b65781a2ca234977d8744dd6925c5d4
ManasijaBhargavan <manas@ucsb.edu>
2025-06-01 16:20:30 -0700

    bruh

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -155,7 +155,7 @@ int main(void)
         BSE_APPS_violation = true;
         sendBseAppsViolationMessage();
     }
-    uint16_t rearThrottleRequest = (uint16_t)((pedalTravel - 0.05) / 0.95 * MAX_CURRENT_REAR * 10) << 8;
+    uint16_t rearThrottleRequest = pedalTravel > 0.05 ? (uint16_t)((pedalTravel - 0.05) / 0.95 * MAX_CURRENT_REAR * 10) << 8 : 0;
     uint16_t forwardThrottleRequest = (uint16_t)(((pedalTravel - 0.05) / 0.95 * MAX_CURRENT_FORWARD  + 327.69) * 100);
     writeDtiMessage(MSG_DTI_CONTROL_12, (uint8_t*)&driveActive, 1);
     writeDtiMessage(MSG_DTI_CONTROL_1, (uint8_t*)&rearThrottleRequest, 2);
