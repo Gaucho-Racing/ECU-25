2e65f06bc266cce1eefc4f9ac695d75ae1075911
Daniel Hansen <dchansen06@gmail.com>
2025-06-01 17:56:54 -0700

    Non-relative current works great -- no issues

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -156,13 +156,13 @@ int main(void)
         sendBseAppsViolationMessage();
     }
 
-    uint16_t maxCurrent = (uint16_t)MAX_CURRENT_TESTING_AT_OWEN_AND_VAMSI_HOUSE * 10;
+    uint16_t maxCurrent = (uint16_t)MAX_CURRENT_TESTING_AT_OWEN_AND_VAMSI_HOUSE * 10; // Keep as a safety metric I think
     writeDtiMessage(MSG_DTI_CONTROL_8, (uint8_t*)&maxCurrent, 2);
 
     pedalTravel = pedalTravel < 0.05 ? 0 : (pedalTravel - 0.05) / 0.95;
-    uint16_t rearThrottleRequest = (uint16_t)(pedalTravel * 100) << 8;
+    uint16_t rearThrottleRequest = (uint16_t)(pedalTravel * MAX_CURRENT_TESTING_AT_OWEN_AND_VAMSI_HOUSE * 10) << 8;
     writeDtiMessage(MSG_DTI_CONTROL_12, (uint8_t*)&driveActive, 1);
-    writeDtiMessage(MSG_DTI_CONTROL_5, (uint8_t*)&rearThrottleRequest, 2);
+    writeDtiMessage(MSG_DTI_CONTROL_1, (uint8_t*)&rearThrottleRequest, 2);
 
     float x = (getThrottle2() - getThrottle1() * 1.9932988878 - 0.125125991408) / getThrottle2();
     x *= 100;
