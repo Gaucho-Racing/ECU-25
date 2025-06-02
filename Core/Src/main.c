6917293bade9ce73058554f1dec454e65b1ab4fe
Daniel Hansen <dchansen06@gmail.com>
2025-06-01 17:46:43 -0700

    Relative seems to be working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -32,6 +32,7 @@
 #include "customIDs.h"
 #include "utils.h"
 #include "driving.h"
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -155,9 +156,11 @@ int main(void)
         sendBseAppsViolationMessage();
     }
     pedalTravel = pedalTravel < 0.05 ? 0 : (pedalTravel - 0.05) / 0.95;
-    uint16_t rearThrottleRequest = (uint16_t)(pedalTravel * MAX_CURRENT_TESTING_AT_OWEN_AND_VAMSI_HOUSE * 10) << 8;
+    uint16_t rearThrottleRequest = ((uint16_t)floorf(pedalTravel * 100 * 0.5) * 2) << 8;
     writeDtiMessage(MSG_DTI_CONTROL_12, (uint8_t*)&driveActive, 1);
     writeDtiMessage(MSG_DTI_CONTROL_5, (uint8_t*)&rearThrottleRequest, 2);
+    uint16_t temp = (uint16_t)MAX_CURRENT_TESTING_AT_OWEN_AND_VAMSI_HOUSE * 10;
+    writeDtiMessage(MSG_DTI_CONTROL_8, (uint8_t*)&temp, 2);
 
     float x = (getThrottle2() - getThrottle1() * 1.9932988878 - 0.125125991408) / getThrottle2();
     x *= 100;
