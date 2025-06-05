637d35e4aaf8ee4a6191b7f4b636302e363fac8d
Daniel Hansen <dchansen06@gmail.com>
2025-06-04 20:30:19 -0700

    Adjust timing to a ~30 Hz ECU Status Msg

--- Application/Src/stateMachine.c
+++ Application/Src/stateMachine.c
@@ -24,7 +24,7 @@ volatile uint8_t numberOfBadMessages = 0;
 int32_t dischargeStartMillis = BAD_TIME_Negative1;
 uint16_t lastECUStatusMsgTick = 0;
 
-static const uint16_t howOftenToSpamECUStatusMsgs = 250;    // Might change
+static const uint16_t howOftenToSpamECUStatusMsgs = 35;
 
 void stateMachineTick(void)
 {
