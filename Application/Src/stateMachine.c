1ff322cfa5616ac983f0e10497fb80ad9bb356cf
ManasijaBhargavan <manas@ucsb.edu>
2025-06-06 14:36:42 -0700

    comment out shit messAGE

--- Application/Src/stateMachine.c
+++ Application/Src/stateMachine.c
@@ -102,7 +102,7 @@ void stateMachineTick(void)
 
         //writeMessage(DataBusCAN, MSG_DASH_WARNING_FLAGS, GR_DASH_PANEL, /*FIXME*/);
         writeMessage(DataBusCAN, MSG_ECU_PING_INFORMATION, GR_STEERING_WHEEL, correctlyScaledValues.StatusBits, 3);
-        writeMessage(DataBusCAN, MSG_ECU_PEDALS_DATA, GR_TCM, correctlyScaledValues.ECUPedalDataMsg, 8);
+        //writeMessage(DataBusCAN, MSG_ECU_PEDALS_DATA, GR_TCM, correctlyScaledValues.ECUPedalDataMsg, 8);
     }
 }
 
