8114b00b91ab7aeda8f1f518bc814a76eb026a94
ManasijaBhargavan <manas@ucsb.edu>
2025-06-06 12:01:14 -0700

    fixed switch

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -176,8 +176,6 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
                         globalStatus.ECUState = PRECHARGE_COMPLETE;
                     }
 
-                    break;
-
                 default:
                     if (getBit(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 1) == 0x0 || (getBit(acuMsgTwo->Precharge_Error_IR_State_Software_Latch_Bits, 2) == 0x00 && globalStatus.ECUState != PRECHARGING))
                     {
