38e9776c0a4ba9ff915e79c3ee7c1aa9781b6473
Daniel Hansen <dchansen06@gmail.com>
2025-06-09 20:49:21 -0700

    Steering wheel offline

--- Application/Src/stateMachine.c
+++ Application/Src/stateMachine.c
@@ -103,7 +103,7 @@ void stateMachineTick(void)
         LOGOMATIC("--Global Status Dump--\n");
 
         //writeMessage(DataBusCAN, MSG_DASH_WARNING_FLAGS, GR_DASH_PANEL, /*FIXME*/);
-        writeMessage(DataBusCAN, MSG_ECU_PING_INFORMATION, GR_STEERING_WHEEL, correctlyScaledValues.StatusBits, 3);
+        // writeMessage(DataBusCAN, MSG_ECU_PING_INFORMATION, GR_STEERING_WHEEL, correctlyScaledValues.StatusBits, 3);
         //writeMessage(DataBusCAN, MSG_ECU_PEDALS_DATA, GR_TCM, correctlyScaledValues.ECUPedalDataMsg, 10);
     }
 }
